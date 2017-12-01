c5f4fae5cae1d4b3379045ed02218daeb5e2f6ef
Thighs of a Legend <gregheff@gmail.com>
2017-12-01 11:09:12 -0500

    Look at player script, updated business man prefab.

--- Assets/LookAtPlayer.cs
+++ Assets/LookAtPlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,8 @@ public class LookAtPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		transform.LookAt(Arms.Instance.transform);
 		
 	}
 }
