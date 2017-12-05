417473a74a8d589c26b390c7941600c426d3e271
Charlie Tran <hi@charlietran.com>
2017-12-05 17:56:05 -0500

    Add Instructions screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
-	public AudioSource introMusic;
-	public UnityEngine.UI.Text startText;
-	public float startTextBlinkSpeed = 0.5f;
+    public AudioSource introMusic;
+    public UnityEngine.UI.Text startText;
+    public float startTextBlinkSpeed = 0.5f;
+    bool showingInstructions = false;
+    public CanvasGroup introCanvas;
+    public CanvasGroup instructionsCanvas;
 
-	// Use this for initialization
-	void Start () {
-		StartCoroutine(BlinkStartText());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			SceneManager.LoadScene("Game");
-		}
-		
-	}
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(BlinkStartText());
+        introCanvas.alpha = 1;
+        instructionsCanvas.alpha = 0;
+    }
 
-	IEnumerator BlinkStartText() {
-		bool displayText = true;
-		while (true) {
-			startText.enabled = displayText;
-			displayText = !displayText;
-			yield return new WaitForSeconds(startTextBlinkSpeed);
-		}
-	}
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (showingInstructions) {
+                SceneManager.LoadScene("Game");
+            } else {
+                introCanvas.alpha = 0;
+                instructionsCanvas.alpha = 1;
+				showingInstructions = true;
+            }
+        }
+
+    }
+
+    IEnumerator BlinkStartText() {
+        bool displayText = true;
+        while (true) {
+            startText.enabled = displayText;
+            displayText = !displayText;
+            yield return new WaitForSeconds(startTextBlinkSpeed);
+        }
+    }
 }
--- Assets/Scripts/Phases/WorkPhase.cs
+++ Assets/Scripts/Phases/WorkPhase.cs
@@ -71,7 +71,7 @@ public class WorkPhase : Phase {
     }
 
     IEnumerator ReturnToStart() {
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(3.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
     }
 
